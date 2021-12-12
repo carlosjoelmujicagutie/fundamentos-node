d0d25bd7252ce3cdeb3ce74d14315c892be8a003
carlos joel mujica <carlosjoelmujica@gmail.com>
2021-12-12 15:14:53 -0500

    segundo commit

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.get('/servicios',(req, res) => {
 app.get('/nosotros',(req, res) => {
   res.send('estas en la pagina de noso')
 })
+app.get('/acerca de',(req, res) => {
+  res.send('estas en la pagina de acerca')
+})
 app.use((req, res, next) => {
   res.status(404).sendFile(__dirname +"/public/404.html")
 })
